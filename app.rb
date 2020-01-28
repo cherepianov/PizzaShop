2fc050e28775407a9fd44b8a5a0f1c989992c06b
Cherepianov <cherepianov@mail.ru>
2020-01-28 14:51:01 +0300

    add migration

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'active_record/version'
+require 'sinatra/activerecord'
 
 set :database, "sqlite3:pizzashop.db"
 
-class Product < Activerecord::base
-  
+class Product < ActiveRecord::Base
+
 end
 
 get '/' do
